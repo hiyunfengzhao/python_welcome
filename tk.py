b12b4c3337483a5efd8962f8ee4ba0af50f49142
hiyunfengzhao <27982597+hiyunfengzhao@users.noreply.github.com>
2020-04-22 16:01:57 -0700

    update

--- tk.py
+++ tk.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from tkinter import messagebox
+import pickle
 # create a window
 window = tk.Tk()
 window.title('Welcome to xxx')
@@ -17,8 +19,9 @@ tk.Label(window, text='Password: ').place(x=50, y=190)
 
 # initialize default value
 var_user_name = tk.StringVar()
-var_user_pass = tk.StringVar()
 var_user_name.set('example@gmail.com')
+var_user_pass = tk.StringVar()
+
 # entry box
 entry_user_name = tk.Entry(window, textvariable=var_user_name)
 entry_user_name.place(x=160,y=150)
@@ -28,14 +31,36 @@ entry_user_pass.place(x=160,y=190)
 # function
 
 def user_login():
-    pass
+    user_name = var_user_name.get()
+    user_pass = var_user_pass.get()
+    try:
+        with open('usrs_info.pickle','rb') as usr_file:
+            usrs_info = pickle.load(usr_file)
+    except FileNotFoundError:
+        with open('usrs_info.pickle','wb') as usr_file:
+            usrs_info = {'admin':'admin'}
+            pickle.dump(usrs_info,usr_file)
+    
+    if user_name in usrs_info:
+        if user_pass == usrs_info[user_name]:
+            tk.messagebox.showinfo(message='Welcome ' + user_name)
+        else:
+            tk.messagebox.showerror(message='Password incorrect / User not found')
+    else:
+        is_sign_up = tk.messagebox.askyesno('You have not signed up, do you want to sign up?')
+        
+        if is_sign_up:
+            user_sign_up()
+
+
+
 def user_sign_up():
-    pass
+    window_sign_up = tk.Toplevel(window)
 
 # button
-btn_login = tk.Button(window, text='Login',command='user_login')
+btn_login = tk.Button(window, text='Login',command=user_login)
 btn_login.place(x=100,y=230)
-btn_sign_up = tk.Button(window,text='Sign up',command='user_sign_up')
+btn_sign_up = tk.Button(window,text='Sign up',command=user_sign_up)
 btn_sign_up.place(x=200,y=230)
 
 # refresh window
